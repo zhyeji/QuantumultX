afbada69d90a695e7fc574ed45bc0ad6131924f9
zhyeji <148704953+zhyeji@users.noreply.github.com>
2026-05-12 02:43:35 +0800

    Update WeTalk_3.js

--- WeTalk_3.js
+++ WeTalk_3.js
@@ -1,12 +1,14 @@
-var info = '--- 测试开始 ---\n';
-info += 'typeof $trigger: ' + (typeof $trigger) + '\n';
-info += '$trigger 值: [' + ($trigger === undefined ? 'undefined' : String($trigger)) + ']\n';
-info += '$trigger === "": ' + ($trigger === '') + '\n';
-info += '类型检查: ' + (typeof $trigger === 'string' ? '是字符串' : '不是字符串') + '\n';
+// 记录脚本刚被执行时的“启动时刻”
+var startTime = Date.now();
 
-if (typeof $trigger === 'string') {
-  info += '包含 "time": ' + ($trigger.indexOf('time') >= 0) + '\n';
-}
+// 模拟一点点启动过程 (这里只是做个标记)
+var initTime = Date.now();
 
-$notify('WeTalk 触发测试', '', info);
+// 计算从脚本开始执行,到真正执行到这行代码的耗时(毫秒)
+var startDelay = initTime - startTime;
+
+var info = '启动耗时(ms): ' + startDelay + '\n\n';
+info += '如果这个值手动和定时差别大(如 >50ms),\n我们就可以用这个值来判断!';
+
+$notify('WeTalk 时间差测试', '', info);
 $done();
