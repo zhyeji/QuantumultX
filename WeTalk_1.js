796c3ea54a025f2f18cfd2eeec2246cee4fb377c
zhyeji <148704953+zhyeji@users.noreply.github.com>
2026-05-15 08:43:49 +0800

    Update WeTalk_1.js

--- WeTalk_1.js
+++ WeTalk_1.js
@@ -2,9 +2,9 @@
 @Name：WeTalk 自动化签到+视频奖励
 @Desc：自动签到+领视频奖励，累计当日数据，格式化输出 (ES5 兼容最终版)
 [rewrite_local]
-^https:\/\/api\.wetalkapp\.com\/app\/queryBalanceAndBonus url script-request-header https://raw.githubusercontent.com/zhyeji/QuantumultX/main/WeTalk_2.js
+^https:\/\/api\.wetalkapp\.com\/app\/queryBalanceAndBonus url script-request-header https://raw.githubusercontent.com/zhyeji/QuantumultX/main/WeTalk_1.js
 [task_local]
-* * * * * https://raw.githubusercontent.com/zhyeji/QuantumultX/main/WeTalk_2.js, tag=WeTalk签到, enabled=true
+* * * * * https://raw.githubusercontent.com/zhyeji/QuantumultX/main/WeTalk_1.js, tag=WeTalk签到, enabled=true
 [MITM]
 hostname = api.wetalkapp.com
 */
